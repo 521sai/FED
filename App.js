81b73db4c496ce159362c2c85e4e001c0db19c98
sivangula sai <saisivangula242@gmail.com>
2022-10-11 17:56:30 +0530

    this is react app
    react app

---
+++ App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Assignment } from './Assignment/Assignment.js';
+
+function App() {
+ return (
+    <div >
+        <Assignment
+        />
+    </div>
+ );
+}
+export default App;
+
